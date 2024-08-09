4a4b1276133b44be0cbc0f1911da25d9463d7410
LeventJamesCoding <leventenespiskin@gmail.com>
2024-08-09 16:02:47 +0300

    Add files via upload

---
+++ Height Checker/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Height Checker
+//Difficulty: Easy
+//A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+//You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+//Return the number of indices where heights[i] != expected[i].
+
+
+
+//Example 1:
+
+//Input: heights = [1,1,4,2,1,3]
+//Output: 3
+//Explanation: 
+//heights:  [1,1,4,2,1,3]
+//expected: [1,1,1,2,3,4]
+//Indices 2, 4, and 5 do not match.
+//Example 2:
+
+//Input: heights = [5,1,2,3,4]
+//Output: 5
+//Explanation:
+//heights:  [5,1,2,3,4]
+//expected: [1,2,3,4,5]
+//All indices do not match.
+//Example 3:
+
+//Input: heights = [1,2,3,4,5]
+//Output: 0
+//Explanation:
+//heights:  [1,2,3,4,5]
+//expected: [1,2,3,4,5]
+//All indices match.
+
+
+//Constraints:
+
+//1 <= heights.length <= 100
+//1 <= heights[i] <= 100A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+//You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+//Return the number of indices where heights[i] != expected[i].
+
+
+
+//Example 1:
+
+//Input: heights = [1,1,4,2,1,3]
+//Output: 3
+//Explanation: 
+//heights:  [1,1,4,2,1,3]
+//expected: [1,1,1,2,3,4]
+//Indices 2, 4, and 5 do not match.
+//Example 2:
+
+//Input: heights = [5,1,2,3,4]
+//Output: 5
+//Explanation:
+//heights:  [5,1,2,3,4]
+//expected: [1,2,3,4,5]
+//All indices do not match.
+//Example 3:
+
+//Input: heights = [1,2,3,4,5]
+//Output: 0
+//Explanation:
+//heights:  [1,2,3,4,5]
+//expected: [1,2,3,4,5]
+//All indices match.
+
+
+//Constraints:
+
+//1 <= heights.length <= 100
+//1 <= heights[i] <= 100
+namespace Height_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.ReadLine();
+        }
+        public class Solution
+        {
+            private int[] heights1 = new int[] { 1, 1, 4, 2, 1, 3 };
+            private int[] heights2 = new int[] { 5, 1, 2, 3, 4 };
+            public Solution()
+            {
+                Console.WriteLine($"Input = [1,1,4,2,1,3]; Output = {HeightChecker(heights1)}, Expected = 3");
+                Console.WriteLine($"Input = [5,1,2,3,4]; Output = {HeightChecker(heights2)}, Expected = 5");
+            }
+            public int HeightChecker(int[] heights)
+            {
+                int[] _heights = new int[heights.Length];
+
+                for (int i = 0; i < heights.Length; i++)
+                    _heights[i] = heights[i];
+
+                Array.Sort(heights);
+                int counter = 0;
+
+                for (int i = 0; i < heights.Length; i++)
+                {
+                    if (_heights[i] != heights[i])
+                        counter++;
+                }
+                return counter;
+            }
+        }
+    }
+}
---
+++ Kth Distinct String in an Array/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Kth Distinct String in an Array
+//Difficulty: Easy
+//A distinct string is a string that is present only once in an array.
+
+//Given an array of strings arr, and an integer k, return the kth distinct string present in arr. If there are fewer than k distinct strings, return an empty string "".
+
+//Note that the strings are considered in the order in which they appear in the array.
+
+ 
+
+//Example 1:
+
+//Input: arr = ["d", "b", "c", "b", "c", "a"], k = 2
+//Output: "a"
+//Explanation:
+//The only distinct strings in arr are "d" and "a".
+//"d" appears 1st, so it is the 1st distinct string.
+//"a" appears 2nd, so it is the 2nd distinct string.
+//Since k == 2, "a" is returned. 
+//Example 2:
+
+//Input: arr = ["aaa", "aa", "a"], k = 1
+//Output: "aaa"
+//Explanation:
+//All strings in arr are distinct, so the 1st string "aaa" is returned.
+//Example 3:
+
+//Input: arr = ["a", "b", "a"], k = 3
+//Output: ""
+//Explanation:
+//The only distinct string is "b". Since there are fewer than 3 distinct strings, we return an empty string "".
+ 
+
+//Constraints:
+
+//1 <= k <= arr.length <= 1000
+//1 <= arr[i].length <= 5
+//arr[i] consists of lowercase English letters.
+namespace Kth_Distinct_String_in_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.ReadLine();
+        }
+        public class Solution
+        {
+            private string[] arr1 = { "d", "b", "c", "b", "c", "a" };
+            private string[] arr2 = { "aaa", "aa", "a" };
+            private string[] arr3 = { "a", "b", "a" };
+            int k1 = 2, k2 = 1, k3 = 3;
+            public Solution()
+            {
+                Console.WriteLine($"Inputs: arr = [\"d\",\"b\",\"c\",\"b\",\"c\",\"a\"], k = 2; Output = \"{KthDistinct(arr1, k1)}\", Expected = \"a\"");
+                Console.WriteLine($"Inputs: arr = [\"aaa\",\"aa\",\"a\"], k = 1; Output = {KthDistinct(arr2, k2)}, Expected = \"aaa\"");
+                Console.WriteLine($"Inputs: arr = [\"a\",\"b\",\"a\"], k = 3; Output = {KthDistinct(arr3, k3)}, Expected = \"\"");
+            }
+            public string KthDistinct(string[] arr, int k)
+            {
+                List<string> list = new List<string>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    int frequency = 0;
+                    for (int j = 0; j < arr.Length; j++)
+                    {
+                        if (arr[i] == arr[j])
+                            frequency++;
+                    }
+                    if (frequency == 1)
+                    {
+                        list.Add(arr[i]);
+                    }
+                }
+                if (k > list.Count)
+                    return "";
+
+                return list[k - 1];
+            }
+        }
+    }
+}
