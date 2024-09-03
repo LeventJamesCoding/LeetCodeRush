153e77d4aa650f8ee5840ec8c8c07c9336c052a4
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-09-03 13:28:21 +0300

    Add files via upload

---
+++ Number of Senior Citizens/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Number of Senior Citizens
+//Difficulty: Easy
+//You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+//The first ten characters consist of the phone number of passengers.
+//The next character denotes the gender of the person.
+//The following two characters are used to indicate the age of the person.
+//The last two characters determine the seat allotted to that person.
+//Return the number of passengers who are strictly more than 60 years old.
+
+
+
+//Example 1:
+
+//Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+//Output: 2
+//Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+//Example 2:
+
+//Input: details = ["1313579440F2036","2921522980M5644"]
+//Output: 0
+//Explanation: None of the passengers are older than 60.
+
+
+//Constraints:
+
+//1 <= details.length <= 100
+//details[i].length == 15
+//details[i] consists of digits from '0' to '9'.
+//details[i][10] is either 'M' or 'F' or 'O'.
+//The phone numbers and seat numbers of the passengers are distinct.You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+//The first ten characters consist of the phone number of passengers.
+//The next character denotes the gender of the person.
+//The following two characters are used to indicate the age of the person.
+//The last two characters determine the seat allotted to that person.
+//Return the number of passengers who are strictly more than 60 years old.
+
+
+
+//Example 1:
+
+//Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+//Output: 2
+//Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+//Example 2:
+
+//Input: details = ["1313579440F2036","2921522980M5644"]
+//Output: 0
+//Explanation: None of the passengers are older than 60.
+
+
+//Constraints:
+
+//1 <= details.length <= 100
+//details[i].length == 15
+//details[i] consists of digits from '0' to '9'.
+//details[i][10] is either 'M' or 'F' or 'O'.
+//The phone numbers and seat numbers of the passengers are distinct.
+namespace Number_of_Senior_Citizens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.ReadLine();
+        }
+        public class Solution
+        {
+            private string[] details1 = { "7868190130M7522", "5303914400F9211", "9273338290F4010" };
+            private string[] details2 = { "1313579440F2036", "2921522980M5644" };
+            public Solution()
+            {
+                Console.WriteLine($"Input: details = [\"7868190130M7522\",\"5303914400F9211\",\"9273338290F4010\"]; Output = {CountSeniors(details1)}, Expected = 2");
+                Console.WriteLine($"Input: details = [\"1313579440F2036\",\"2921522980M5644\"]; Output = {CountSeniors(details2)}, Expected = 0");
+            }
+            public int CountSeniors(string[] details)
+            {
+                int seniorCounter = 0;
+                for (int i = 0; i < details.Length; i++)
+                {
+                    int ageOfPerson = int.Parse(details[i][11].ToString()) * 10 + int.Parse(details[i][12].ToString());
+                    if (ageOfPerson > 60) seniorCounter++;
+                }
+                return seniorCounter;
+            }
+        }
+    }
+}
