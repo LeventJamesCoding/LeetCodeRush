ad0ab6ec1ff1f3fda616a7412a8c5b4a31cc6b97
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-07 17:43:19 +0300

    Add files via upload

---
+++ Minimum String Length After Removing Substrings/Program.cs
@@ -0,0 +1,61 @@
+﻿//You are given a string s consisting only of uppercase English letters.
+
+//You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+//Return the minimum possible length of the resulting string that you can obtain.
+
+//Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+
+
+//Example 1:
+
+//Input: s = "ABFCACDB"
+//Output: 2
+//Explanation: We can do the following operations:
+//-Remove the substring "ABFCACDB", so s = "FCACDB".
+//- Remove the substring "FCACDB", so s = "FCAB".
+//- Remove the substring "FCAB", so s = "FC".
+//So the resulting length of the string is 2.
+//It can be shown that it is the minimum length that we can obtain.
+//Example 2:
+
+//Input: s = "ACBBD"
+//Output: 5
+//Explanation: We cannot do any operations on the string so the length remains the same.
+
+
+//Constraints:
+
+//1 <= s.length <= 100
+//s consists only of uppercase English letters.
+namespace Minimum_String_Length_After_Removing_Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        private string s1 = "ABFCACDB", s2 = "ACBBD";
+        public Solution()
+        {
+            Console.WriteLine($"Input: s = {s1}; Output = {MinLength(s1)}, Expected = 2");
+            Console.WriteLine($"Input: s = {s2}; Output = {MinLength(s2)}, Expected = 5");
+        }
+        public int MinLength(string s)
+        {
+            while (s.Contains("AB") || s.Contains("CD"))
+            {
+                s = s.Replace("AB", "");
+                s = s.Replace("CD", "");
+            }
+            return s.Length;
+        }
+    }
+}
