bb19e33abb46b76eefec43f40f219c8cf308a969
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-18 16:49:08 +0300

    Add files via upload

---
+++ Ugly Number/Ugly Number/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace UglyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class Solution
+        {
+            public bool IsUgly(int n)
+            {
+                if (n <= 0)
+                    return false;
+
+                int[] limits = { 2, 3, 5 };
+
+                foreach (int limit in limits)
+                {
+                    while (n % limit == 0)
+                    {
+                        n /= limit;
+                    }
+                }
+
+                return n == 1;
+            }
+        }
+
+    }
+}
