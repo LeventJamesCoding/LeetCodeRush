c42056705f53d88ad67e0fbfe8b1edd7452705da
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-18 18:07:34 +0300

    Add files via upload

---
+++ Move Zeroes/Move Zeroes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MoveZeroes
+{
+    internal class Program
+    {
+
+    }
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int zeroCount = 0;
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                if (nums[i] == 0)
+                {
+                    zeroCount++; 
+                }
+                else if (zeroCount > 0)
+                {
+                    nums[i - zeroCount] = nums[i];
+                }
+            }
+            int right = nums.Length - 1; 
+            while (zeroCount > 0)
+            {
+                nums[right] = 0;
+                zeroCount--; 
+                right--; 
+            }
+        }
+    }
+}
