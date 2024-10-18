74cb2c0bd6c206d7e5e2e80fc53b1a784189aa75
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-18 17:05:34 +0300

    Add files via upload

---
+++ Missing Number/Missing Number/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Missing_Number
+{
+    internal class Program
+    {
+
+    }
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            Stack<int> stack = new Stack<int>();
+            int result = 0;
+            for(int i = 0; i < nums.Length+1; i++) 
+            {
+                stack.Push(i);
+            }
+
+            foreach(int number in stack)
+            {
+                if(!nums.Contains(number))
+                {
+                    result = number; 
+                    break;
+                }
+            }
+            return result;  
+        }
+    }
+}
+
