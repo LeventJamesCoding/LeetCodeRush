0a9168e7ff41b106efd1c0208b8c141385273435
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-18 20:42:32 +0300

    Add files via upload

---
+++ Contains Duplicate/Contains Duplicate/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ContainsDuplicate
+{
+    internal class Program
+    {
+
+    }
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums) 
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (int x in nums)
+            {
+                if(!stack.Contains(x))
+                {
+                    stack.Push(x);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+}
+}
