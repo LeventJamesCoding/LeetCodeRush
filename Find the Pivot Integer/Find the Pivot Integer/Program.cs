c476fd924894f93b9f1143b43a862fa0f16514b6
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-23 13:23:24 +0300

    Add files via upload

---
+++ Find the Pivot Integer/Find the Pivot Integer/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Find_the_Pivot_Integer
+{
+    internal class Program
+    {
+
+    }
+    public class Solution
+    {
+        public int PivotInteger(int n)
+        {
+            for(int pivotNumber = 1; pivotNumber <= n; pivotNumber++)
+            {
+                int lowerSum = 0;
+                int upperSum = 0;
+                //calculate lowerSum
+                for(int i = 1; i <= pivotNumber; i++)
+                {
+                    lowerSum += i;
+                }
+                //calculate upperSum
+                for(int i = pivotNumber;i <= n; i++)
+                {
+                    upperSum += i;  
+                }
+                if(upperSum == lowerSum)
+                {
+                    return pivotNumber;
+                }
+            }
+            return -1;
+        }
+    }
+}
