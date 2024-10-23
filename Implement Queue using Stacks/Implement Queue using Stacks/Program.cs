cc3e76175256fa14ed7e05f952b8d4752aacb1d6
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-23 15:18:55 +0300

    Add files via upload

---
+++ Implement Queue using Stacks/Implement Queue using Stacks/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Implement_Queue_using_Stacks
+{
+    public class MyQueue
+    {
+        List<int> stack = new List<int>();
+        public MyQueue()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            stack.Add(x);
+        }
+
+        public int Pop()
+        {
+            int returnedElement = stack[0];
+            stack.RemoveAt(0);
+            return returnedElement;
+        }
+
+        public int Peek()
+        {
+            return stack[0];
+        }
+
+        public bool Empty()
+        {
+            if(stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
