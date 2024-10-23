ccd8d551ef1b82be3b0539280a3213707be6e190
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-23 15:10:38 +0300

    Add files via upload

---
+++ Power of Two/Power of Two/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Power_of_Two
+{
+    internal class Program
+    {
+
+    }
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0) return false;
+            var result = Math.Log2(n);
+            return result == (int)result;
+        }
+    }
+}
