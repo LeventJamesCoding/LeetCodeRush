444cebc76de4adb4a8c830076195061b80d3bea9
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-23 16:22:14 +0300

    Add files via upload

---
+++ First Bad Version/First Bad Version/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace First_Bad_Version
+{
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+    public class Solution : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            //binary search
+            int low = 1;
+            int high = n;
+
+            while (Math.Abs(high - low) > 1)
+            {
+                int mid = low + ((high - low) / 2);
+                bool status = IsBadVersion(mid);
+                if (status == false)
+                {
+                    low = mid;
+                }
+                else
+                {
+                    if (!IsBadVersion(mid - 1))
+                    {
+                        return mid;
+                    }
+                    high = mid;
+                }
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsBadVersion(i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
