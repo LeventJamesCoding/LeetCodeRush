e0c7d94e05f23e3c9ca89e56a47a2bd6860b248f
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-23 12:33:07 +0300

    Add files via upload

---
+++ Convert 1D Array Into 2D Array/Convert 1D Array Into 2D Array/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Convert_1D_Array_Into_2D_Array
+{
+    internal class Program
+    {
+
+    }
+    public class Solution
+    {
+        public int[][] Construct2DArray(int[] original, int m, int n)
+        {
+            List<List<int>> list = new List<List<int>>();
+            if (original.Length != m * n)
+            {
+                return new int[0][];
+            }
+
+            for (int i = 0; i < original.Length; i += n)
+            {
+                List<int> temp = new List<int>();
+                for (int j = i; j < n + i; j++)
+                {
+                    temp.Add(original[j]);
+                }
+                list.Add(temp);
+            }
+            return list.Select(innerList => innerList.ToArray()).ToArray();
+        }
+    }
+}
