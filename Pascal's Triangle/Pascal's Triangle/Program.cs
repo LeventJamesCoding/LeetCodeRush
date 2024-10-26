94befdce230b7307c40e01c5eae3451f126022ed
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-10-26 15:10:42 +0300

    Add files via upload

---
+++ Pascal's Triangle/Pascal's Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Pascals_Triangle
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> result = new List<IList<int>>(); 
+            List<int> temp = new List<int>();
+
+            for(int i = 0; i < numRows; i++)
+            {
+                List<int> row = new List<int>();
+                int size = i + 1;
+
+                for (int j = 0; j < size; j++) 
+                {
+                    if (j == 0 || j == size - 1)
+                    {
+                        row.Add(1);
+                        continue;
+                    }
+                    int numberToAdd = temp[j] + temp[j - 1];
+                    row.Add(numberToAdd);
+                }
+                result.Add(row);
+                temp = row;
+            }
+
+            return result;
+        }
+    }
+}
