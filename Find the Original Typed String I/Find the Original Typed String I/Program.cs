b3d0fef1ce407c4beba694c50d8e8db6cf3f2290
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-11-01 15:25:40 +0300

    Add files via upload

---
+++ Find the Original Typed String I/Find the Original Typed String I/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Find_the_Original_Typed_String_I
+{
+    public class Solution
+    {
+        public int PossibleStringCount(string word)
+        {
+            int result = 0;
+            int count = 1;
+            char previous = word[0];
+
+            for (int i = 1; i < word.Length; i++)
+            {   
+                if(previous != word[i]) 
+                {
+                    if(count > 1)
+                    {
+                        result += count - 1;
+                    }
+                    count = 1;
+                }
+                else 
+                {
+                    count++;
+                }
+                previous = word[i];
+            }
+
+            if(count > 1)
+            {
+                result += count - 1;
+            }
+
+            return result + 1;
+        }
+    }
+}
