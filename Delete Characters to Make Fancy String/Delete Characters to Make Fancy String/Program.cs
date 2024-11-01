2c1525ddb36a1fcb8689a29b41707d0c78c7bd7b
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-11-01 12:48:09 +0300

    Add files via upload

---
+++ Delete Characters to Make Fancy String/Delete Characters to Make Fancy String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+namespace Delete_Characters_to_Make_Fancy_String
+{
+    public class Solution
+    {
+        public string MakeFancyString(string s)
+        {
+            var result = new StringBuilder();
+            int counter = 1;  
+            char previous = s[0];
+            result.Append(previous);
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == previous)
+                {
+                    if (counter < 2)  
+                    {
+                        result.Append(s[i]);
+                    }
+                    counter++;
+                }
+                else
+                {
+                    previous = s[i];
+                    result.Append(s[i]);
+                    counter = 1;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+}
