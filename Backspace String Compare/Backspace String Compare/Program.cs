a2521d831d3fa7285e8fa349923b7c6ef8d30812
Levent Enes Piskin <leventenespiskin@gmail.com>
2024-11-26 13:52:06 +0300

    Add files via upload

---
+++ Backspace String Compare/Backspace String Compare/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Backspace_String_Compare
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public bool BackspaceCompare(string s, string t)
+            {
+                string concreteS = "", concreteT = "";
+
+                foreach(char c in s)
+                {
+                    if(c != '#')
+                    {
+                        concreteS += c;
+                    }
+                    else if(concreteS != "" && c == '#')
+                    {
+                        //concreteS' in son elemanın sil
+                        concreteS = concreteS.Remove(concreteS.Length - 1);
+                    }
+                }
+
+                foreach (char c in t)
+                {
+                    if (c != '#')
+                    {
+                        concreteT += c;
+                    }
+                    else if (concreteT != "" && c == '#')
+                    {
+                        //concreteT' nin son elemanın sil
+                        concreteT = concreteT.Remove(concreteT.Length - 1);
+                    }
+                }
+
+                return concreteS.Equals(concreteT);
+            }
+        }
+    }
+}
